59daad36b64e87f68ee64cced8cc7c5433401e0f
Anand2027 <anandgupta020204@gmail.com>
2026-06-08 21:47:40 +0530

    frontend-deployment

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 import "./App.css";
 
+const API_URL = "https://codescanai-cntt.onrender.com";
+
 export default function App() {
   const [page, setPage] = useState("landing"); // "landing" | "app"
   const [review, setReview] = useState(null);
@@ -25,7 +27,8 @@ export default function App() {
     setOriginalLang(language);
 
     try {
-      const response = await fetch("/api/review", {
+      // const response = await fetch("/api/review", {
+      const response = await fetch(`${API_URL}/api/review`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ code, language, features }),
@@ -59,7 +62,8 @@ export default function App() {
     setActiveTab("fixed");
 
     try {
-      const response = await fetch("/api/fix", {
+      // const response = await fetch("/api/fix", {
+      const response = await fetch(`${API_URL}/api/fix`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ code: originalCode, language: originalLang, review }),
